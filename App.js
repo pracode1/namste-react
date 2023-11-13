4aa48f75f8084dd3ea7ccd579430c0403b9eabf5
pracode <prachipateltech@gmail.com>
2023-11-13 13:37:13 +0530

    episode-2

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-const heading = React.createElement("div", { }, 
-[React.createElement("div", { }, 
-[React.createElement("h1", { }, "Hello from H1"),
-React.createElement("h2", { }, "Hello from H2")]),
-React.createElement("div", { }, 
-[React.createElement("h1", { }, "Hello from H1"),
-React.createElement("h2", { }, "Hello from H2")])]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-root.render(heading)
+const heading = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "I'm an h1 tag"),
+      React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "I'm an h1 tag"),
+      React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+  ]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
